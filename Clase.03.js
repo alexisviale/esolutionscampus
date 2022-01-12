734e9260647f5387da4f1ef4865456ffa94e10f1
Alexis Viale <alexisviale@ALEXIS-5.local>
2022-01-12 11:15:21 -0300

    Ejercicio TYPE OF

--- Clase.03.js
+++ Clase.03.js
@@ -3,3 +3,82 @@ function add(a, b) {
     console.log(`${a} + ${b} = ${sum}`);
 }
 
+function mult(a, b) {
+    let product = a * b;
+    console.log(`${a} * ${b} = ${sum}`);
+}
+
+let sum = add
+
+//Copia de la función
+sum(100, 20)
+
+//Llamado a la función
+add(30, 40)
+
+const arr = [add, mult]
+
+arr[0](2, 5)
+
+let number = Number.parseInt(`8`)
+
+console.log(number)
+
+function showAccept() {
+    console.log('Accepted');
+}
+
+function showCancel() {
+    console.log('Canceled');
+}
+
+function process(question, yes, no) {
+    confirm(question) ? yes() : no();
+}
+
+process('Are you sure?', showAccept, showAccept)
+
+
+//Funciones anidadas
+
+function firstLevel() {
+    console.log('Primer nivel...');
+    const arr = []
+    function secondLevel() {
+        console.log('Segundo nivel...');
+        arr.push('dos')
+        thirdLevel()
+        console.log('En el segundo nivel -> ' + arr);
+        arr.pop()
+        function thirdLevel() {
+            console.log('Tercer nivel...');
+            arr.push('tres')
+            console.log('En el tercer nivel -> ' + arr);
+        }
+    }
+    secondLevel()
+    console.log('En el primer nivel -> ' + arr);
+}
+
+firstLevel()
+
+//Arguments
+
+function selectFirst() {
+    console.log('El primer elemento es: ' + arguments[0]);
+}
+
+selectFirst('A', 'B', 'C')
+
+//Ejercicio
+
+function argumType(param1) {
+    return (typeof param1)
+}
+
+function primitive() {
+    let result = argumType(10)
+    console.log(result);
+}
+
+primitive()
