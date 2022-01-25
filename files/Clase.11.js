87b177886b9e3f33f5ddaf5b6fdbf105d9f61905
Alexis Viale <alexisviale@ALEXIS-5.local>
2022-01-25 12:03:34 -0300

    Clase 11 y 12

--- files/Clase.11.js
+++ files/Clase.11.js
@@ -1,2 +1,6 @@
 //Procesos secundarios en node.js
 
+//Express
+
+
+
---
+++ files/web-service/models/index.js
@@ -0,0 +1,28 @@
+let users = {
+    1: {
+        id: '1',
+        username: 'Homero Simpson'
+    },
+    2: {
+        id: '2',
+        username: 'Ned Flanders'
+    }
+}
+
+let messages = {
+    1: {
+        id: '1',
+        text: 'Mmmm... rosquillas...',
+        userId: '1'
+    },
+    2: {
+        id: '2',
+        text: 'Perfectirihillo',
+        userId: '2'
+    }
+}
+
+module.exports = {
+    users,
+    messages
+}
